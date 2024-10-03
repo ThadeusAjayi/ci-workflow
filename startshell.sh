d23d0cd939d33be4cbd73af055ad802561de4ebb
Thadeus Ajayi <ainojie@gmail.com>
2024-10-03 15:07:49 +0100

    remove sudo

--- startshell.sh
+++ startshell.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 npm install
 
 node index.js
